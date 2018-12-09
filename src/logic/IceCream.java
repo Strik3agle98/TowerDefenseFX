5ce9799767f3cc9b6107b1d54ee0fb3588aa19f6
Anan Methasate <anan_m@me.com>
2018-12-09 13:10:07 +0700

    Fixing name

--- src/logic/IceCream.java
+++ src/logic/IceCream.java
@@ -5,7 +5,7 @@ import javafx.scene.image.Image;
 public class IceCream extends ShootingTower{
 	public IceCream(int row,int col) {
 		super("Ice-Cream",100,50);
-		super.setImage(new Image("Tower2_Attacking.gif"),new Image("Tower2_standby.gif"));
+		super.setImage(new Image("Tower2_Attacking.gif"),new Image("Tower2_Standby.gif"));
 		super.setRow(row);
 		super.setCol(col);
 	}
