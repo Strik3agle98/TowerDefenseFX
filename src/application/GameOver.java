bed590ffd7d02fc2b2c3ac7e0f770ee665b29b97
Woraprat Saelo <can_saelo@hotmail.com>
2018-12-10 15:11:01 +0700

    implement gameover

--- src/application/GameOver.java
+++ src/application/GameOver.java
@@ -1,15 +1,34 @@
 package application;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Cursor;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 
-public class GameOver extends GridPane{
+public class GameOver extends VBox{
+	private Button proceed;
+	
 	public GameOver() {
+		setAlignment(Pos.CENTER);
 		setPrefSize(GUI.W,GUI.H);
-		Canvas c = new Canvas(GUI.W,GUI.H);
-		c.setStyle("-fx-background-image: url('icecream4.gif')");
-		c.getGraphicsContext2D().fillText("Game Over", GUI.W / 2, GUI.H / 2);
-		getChildren().add(c);
+		//Canvas c = new Canvas(GUI.W,GUI.H);
+		setStyle("-fx-background-image: url('GameOver.jpg')");
+		//c.getGraphicsContext2D().fillText("Game Over", GUI.W / 2, GUI.H / 2);
+		proceed = new Button("");
+		proceed.setPrefSize(200, 50);
+		proceed.setStyle("-fx-background-image: url('PROCEED.png')");
+		proceed.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent arg0) {
+				System.out.println("You pressed proceed.");
+				GUI.getStage().setScene(GUI.getSceneOfMainMenu());
+			}
+		});
+		proceed.setCursor(Cursor.HAND);
+		getChildren().add(proceed);
 	}
 	
 }
